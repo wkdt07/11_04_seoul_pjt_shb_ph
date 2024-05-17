572123dec5d3818e0cdd18a19e04800a8be8cf00
unknown <h2015304023@office.kw.ac.kr>
2024-05-17 20:47:47 +0900

    Depositlist

---
+++ front/vue-project/src/components/DepositListView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const API_URL = 'http://127.0.0.1:8000'
+
+</script>
+
+<style scoped>
+
+</style>
